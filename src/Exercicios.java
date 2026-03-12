7bf2f2e73b474ddcf4aef3ffc291f4ab5e4d2126
Rafael Moura Ferreira <rafaelmoura202050@gmail.com>
2026-03-11 23:55:17 -0300

    Primeiros exercícios de Java

---
+++ src/Exercicios.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class Exercicios {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("O horário atual é " + relogio);
+    }
+}
---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+public class PrimeiroPrograma {
+    public static void main() {
+        /*int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2)/2;
+        System.out.println("A média é igual a " + media);*/
+
+        int numero = 10; // pré/pós incremento e pré/pós decremento
+        int valor = 4 + --numero;
+        System.out.println(valor);
+        System.out.println(numero);
+    }
+}
